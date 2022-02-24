2f61815a6a83bc74946e968556c5522930bf2791
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-02-24 10:08:17 +0100

    feat: Implemented Vector4

---
+++ src/utils/vectors/vector4.cc
@@ -0,0 +1,55 @@
+#include "vector4.hh"
+
+#include <ostream>
+#include <string>
+
+namespace vectors
+{
+    Vector4 Vector4::unit()
+    {
+        return Vector4{ 1, 1, 1, 1 };
+    }
+
+    Vector4 Vector4::zero()
+    {
+        return Vector4{ 0, 0, 0, 0 };
+    }
+
+    Vector4::Vector4(double x, double y, double z, double w)
+        : x{ x }
+        , y{ y }
+        , z{ z }
+        , w{ w }
+    {}
+
+    double Vector4::dot(const Vector4 &other)
+    {
+        return this->x * other.x + this->y * other.y + this->z * other.z
+            + this->w * other.w;
+    }
+
+    Vector4 Vector4::operator*(const double &scalar)
+    {
+        return Vector4{ this->x * scalar, this->y * scalar, this->z * scalar,
+                        this->w * scalar };
+    }
+
+    Vector4 Vector4::operator+(const Vector4 &other)
+    {
+        return Vector4{ this->x + other.x, this->y + other.y, this->z + other.z,
+                        this->w + other.w };
+    }
+
+    Vector4 Vector4::operator-(const Vector4 &other)
+    {
+        return Vector4{ this->x - other.x, this->y - other.y, this->z - other.z,
+                        this->w - other.w };
+    }
+
+    std::ostream &operator<<(const std::ostream &out, Vector4 &vect)
+    {
+        return out << "Vector4{" << vect.x << ", " << vect.y << ", " << vect.z
+                   << ", " << vect.w << "}";
+    }
+
+} // namespace vectors
---
+++ src/utils/vectors/vector4.hh
@@ -0,0 +1,27 @@
+#pragma once
+#include <ostream>
+
+namespace vectors
+{
+    class Vector4
+    {
+    public:
+    double w;
+        double x;
+        double y;
+        double z;
+
+        static Vector4 unit();
+        static Vector4 zero();
+
+        Vector4(double x, double y, double z, double w);
+
+        double dot(const Vector4 &other);
+
+        Vector4 operator+(const Vector4 &other);
+        Vector4 operator-(const Vector4 &other);
+        Vector4 operator*(const double &scalar);
+
+    };
+    std::ostream& operator<<(const std::ostream& out, Vector4 &vect);
+} // namespace vectors
