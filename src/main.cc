a5910a37319cfa4d7cdd2f4d5fd706d08f66651f
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-02-24 13:38:21 +0100

    refactor: Removed debug file creation from main

--- src/main.cc
+++ src/main.cc
@@ -21,15 +21,5 @@ int main(int argc, char *argv[])
     std::cout << "pixel 1,0 = " << img.pixel_get(1, 0) << std::endl;
 
     img.save_ppm("./test.ppm");
-    std::ofstream debug_file("pixels.txt", std::ios::out | std::ios::trunc);
-    for (size_t y = 0; y < height; ++y)
-    {
-        for (size_t x = 0; x < width; ++x)
-        {
-            debug_file << img.pixel_get(x, y) << (x == (width - 1) ? "" : ", ");
-        }
-        debug_file << "\n";
-    }
-    debug_file.close();
     return 0;
 }
