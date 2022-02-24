0084a3f111c64d28e646a019f8857d3d6f315e8b
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-02-24 10:07:55 +0100

    feat: Implemented Vector3

---
+++ src/utils/vectors/vector3.cc
@@ -0,0 +1,52 @@
+#include "vector3.hh"
+
+#include <ostream>
+#include <string>
+
+namespace vectors
+{
+    Vector3 Vector3::unit()
+    {
+        return Vector3{ 1, 1, 1 };
+    }
+
+    Vector3 Vector3::zero()
+    {
+        return Vector3{ 0, 0, 0 };
+    }
+
+    Vector3::Vector3(double x, double y, double z)
+        : x{ x }
+        , y{ y }
+        , z{ z }
+    {}
+
+    double Vector3::dot(const Vector3 &other)
+    {
+        return this->x * other.x + this->y * other.y + this->z * other.z;
+    }
+
+    Vector3 Vector3::operator*(const double &scalar)
+    {
+        return Vector3{ this->x * scalar, this->y * scalar, this->z * scalar };
+    }
+
+    Vector3 Vector3::operator+(const Vector3 &other)
+    {
+        return Vector3{ this->x + other.x, this->y + other.y,
+                        this->z + other.z };
+    }
+
+    Vector3 Vector3::operator-(const Vector3 &other)
+    {
+        return Vector3{ this->x - other.x, this->y - other.y,
+                        this->z - other.z };
+    }
+
+    std::ostream &operator<<(const std::ostream &out, Vector3 &vect)
+    {
+        return out << "Vector3{" << vect.x << ", " << vect.y << ", " << vect.z
+                   << "}";
+    }
+
+} // namespace vectors
---
+++ src/utils/vectors/vector3.hh
@@ -0,0 +1,26 @@
+#pragma once
+#include <ostream>
+
+namespace vectors
+{
+    class Vector3
+    {
+    public:
+        double x;
+        double y;
+        double z;
+
+        static Vector3 unit();
+        static Vector3 zero();
+
+        Vector3(double x, double y, double z);
+
+        double dot(const Vector3 &other);
+
+        Vector3 operator+(const Vector3 &other);
+        Vector3 operator-(const Vector3 &other);
+        Vector3 operator*(const double &scalar);
+
+    };
+    std::ostream& operator<<(const std::ostream& out, Vector3 &vect);
+} // namespace vectors
