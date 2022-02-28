d74e48f97035362fd08d0a6029b5dea0917f92b7
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-02-28 13:59:53 +0100

    feat: Implemented basic raytracing engine (white when intersection)

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@
 
 #include "colors/rgb.hh"
 #include "image/image.hh"
+#include "intersection/intersection.hh"
 #include "lights/light.hh"
 #include "lights/point_light.hh"
 #include "materials/uniform_material.hh"
@@ -14,14 +15,52 @@
 #include "scene/scene.hh"
 #include "vectors/vector3.hh"
 
+using Image = image::Image;
+using namespace raytracer;
+
+std::shared_ptr<Image> raytrace(const Scene &scene, const int &height)
+{
+    using namespace colors;
+    const auto &camera = scene.camera();
+    const auto width =
+        static_cast<int>(std::round(camera.aspectRatio * height));
+    auto output = std::make_shared<Image>(width, height);
+    const auto imagePlaneWidthHalf = std::tan(camera.fov / 2.) * camera.zMin;
+    const auto imagePlaneWidth = imagePlaneWidthHalf * 2;
+    const auto pixelSize = imagePlaneWidth / width;
+    const auto imagePlaneCenter =
+        camera.position + camera.forward * camera.zMin;
+    const auto floatImageWidthHalf = static_cast<double>(width) / 2;
+    const auto floatImageHeightHalf = static_cast<double>(height) / 2;
+
+    for (auto y = 0; y < height; ++y)
+    {
+        for (auto x = 0; x < width; ++x)
+        {
+            const auto rayOrigin = imagePlaneCenter
+                + (x - floatImageWidthHalf) * camera.right * pixelSize
+                + (y - floatImageHeightHalf) * camera.up * pixelSize;
+            // std::cout << "ray origin: " << rayOrigin << "\n";
+            const auto rayDirection = rayOrigin - camera.position;
+
+            const Ray ray{ rayOrigin, rayDirection };
+            auto intersection = scene.cast_ray(ray);
+            if (intersection)
+            {
+                output->pixel_set(x, y, RGB::white());
+            }
+        }
+    }
+
+    return output;
+}
+
 int main(int argc, char *argv[])
 {
-    using Image = image::Image;
-    using namespace raytracer;
     using namespace points;
     using namespace vectors;
-    using namespace raytracer::materials;
-    using namespace raytracer::lights;
+    using namespace materials;
+    using namespace lights;
 
     constexpr int height = 480;
     constexpr double aspectRatio = 16. / 9.;
@@ -32,18 +71,21 @@ int main(int argc, char *argv[])
         Vector3::all(0.7),
         Vector3::all(0.),
     });
+    const auto camPos = Point3::origin();
+    const auto camPoint = Point3(4, 0, 0);
+    const auto sphere = std::make_shared<Sphere>(camPoint, 1., greyUniform);
 
     Scene scene{
         Camera{
-            Point3{ -2, 0, 2 },
-            Point3::origin(),
-            Vector3{ 1, 0, 1 }.normalized(),
+            camPos,
+            camPoint,
+            Vector3::up(),
             fieldOfView,
             aspectRatio,
             0.5,
         },
         Scene::ObjectCollection{
-            std::make_shared<Sphere>(Point3::origin(), 1., greyUniform),
+            sphere,
         },
         Scene::LightCollection{
             std::make_shared<PointLight>(Point3::origin() + Vector3::up() * 3,
@@ -51,6 +93,9 @@ int main(int argc, char *argv[])
         },
     };
 
+    std::cout << "Scene built:" << std::endl;
     std::cout << scene << std::endl;
+    const auto output = raytrace(scene, height);
+    output->save_ppm("result.ppm");
     return 0;
 }
