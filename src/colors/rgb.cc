1bbb179232805bd26ba16ead295a119bef523148
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-03-01 11:36:35 +0100

    refactor: Added color factor from linear color vector

--- src/colors/rgb.cc
+++ src/colors/rgb.cc
@@ -1,5 +1,7 @@
 #include "rgb.hh"
 
+#include <algorithm>
+#include <cmath>
 #include <iostream>
 #include <string>
 
@@ -25,6 +27,23 @@ namespace colors
     RGB RGB::black()
     {
         return RGB{};
+    }
+
+    RGB RGB::from_linear(vectors::Vector3 linearColor)
+    {
+        auto clamped = vectors::Vector3{
+            std::clamp(linearColor.x, 0., 1.),
+            std::clamp(linearColor.y, 0., 1.),
+            std::clamp(linearColor.z, 0., 1.),
+        };
+
+        auto mapped = clamped * 255;
+
+        return RGB{
+            static_cast<unsigned int>(std::round(mapped.x)),
+            static_cast<unsigned int>(std::round(mapped.y)),
+            static_cast<unsigned int>(std::round(mapped.z)),
+        };
     }
 
     std::ostream &operator<<(std::ostream &out, const RGB &color)
--- src/colors/rgb.hh
+++ src/colors/rgb.hh
@@ -2,6 +2,8 @@
 
 #include <ostream>
 
+#include "vectors/vector3.hh"
+
 namespace colors
 {
     struct RGB
@@ -15,6 +17,7 @@ namespace colors
         static RGB gray(unsigned int scale);
         static RGB white();
         static RGB black();
+        static RGB from_linear(vectors::Vector3 linearColor);
     };
 
     std::ostream &operator<<(std::ostream &out, const RGB &color);
