042768dd31374b61f79f9cd22a106768f121a503
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-03-01 15:42:15 +0100

    feat: Added plane object

---
+++ src/objects/plane.cc
@@ -0,0 +1,42 @@
+#include "plane.hh"
+
+#include <cmath>
+
+#include "vectors/vector3.hh"
+
+namespace raytracer::objects
+{
+    Plane::Plane(const points::Point3 &origin, const vectors::Vector3 &normal,
+                 MaterialPtr material)
+        : Object{ material }
+        , origin_{ origin }
+        , normal_{ normal }
+    {}
+
+    std::optional<Intersection> Plane::intersects_ray(const Ray &ray) const
+    {
+        static constexpr auto EPSILON = 0.000000001;
+        const auto denom = normal_.dot(ray.direction);
+        if (std::abs(denom) < EPSILON)
+        {
+            return {}; // parallel
+        }
+        const auto t = (normal_.dot(origin_ - ray.origin)) / denom;
+        if (t < 0)
+        {
+            return {};
+        }
+        return Intersection{ ray, t, 0, 0, this };
+    }
+
+    vectors::Vector3 Plane::get_normal(const Intersection &intersection) const
+    {
+        return normal_;
+    }
+
+    std::ostream &Plane::print(std::ostream &out) const
+    {
+        return out << "Plane{ origin: " << origin_ << ", normal: " << normal_
+                   << " }";
+    }
+} // namespace raytracer::objects
---
+++ src/objects/plane.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <optional>
+
+#include "materials/texture_material.hh"
+#include "object.hh"
+#include "points/point3.hh"
+
+namespace raytracer::objects
+{
+    class Plane : public Object
+    {
+    public:
+        Plane(const points::Point3 &reference, const vectors::Vector3 &normal,
+              MaterialPtr material);
+
+        virtual std::optional<Intersection>
+        intersects_ray(const Ray &ray) const override;
+
+        virtual vectors::Vector3
+        get_normal(const Intersection &intersection) const override;
+
+    private:
+        virtual std::ostream &print(std::ostream &out) const override;
+        
+        points::Point3 origin_;
+        vectors::Vector3 normal_;
+    };
+} // namespace raytracer::objects
