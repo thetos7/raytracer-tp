434e87d25dde500c64d8ed4889fe31a67db45a2d
Ancelin Bouchet <ancelin.bouchet@epita.fr>
2022-03-22 23:09:23 +0100

    fix: Fixed cross product for Vector3

--- src/vectors/vector3.cc
+++ src/vectors/vector3.cc
@@ -60,7 +60,7 @@ namespace vectors
 
     Vector3 Vector3::cross(const Vector3 &other) const
     {
-        return Vector3{ y * other.z - z * other.y, z * other.x - x * other.y,
+        return Vector3{ y * other.z - z * other.y, z * other.x - x * other.z,
                         x * other.y - y * other.x };
     }
 
